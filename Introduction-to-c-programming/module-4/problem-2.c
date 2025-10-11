b97b1b8e04635f5e4f44bd4022d7a81da1a90e2b
ahadahamedakash <a.ahad2232@gmail.com>
2025-10-11 23:37:04 +0600

    Solved codeforces problem no: j

---
+++ Introduction-to-c-programming/module-4/problem-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (a % b == 0 || b % a == 0)
+    {
+        printf("Multiples");
+    }
+    else
+    {
+        printf("No Multiples");
+    }
+
+    return 0;
+}
+
+/*
+    PROBLEM LINK: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/J
+*/
