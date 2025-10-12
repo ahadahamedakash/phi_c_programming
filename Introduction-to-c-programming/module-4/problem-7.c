7767f50f174b8ab6f0176653e8f9d7a64a5b25a9
ahadahamedakash <a.ahad2232@gmail.com>
2025-10-12 11:26:04 +0600

    Solved codeforces problem no: c

---
+++ Introduction-to-c-programming/module-4/problem-7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b;
+    scanf("%lld %lld", &a, &b);
+
+    printf("%lld + %lld = %lld\n", a, b, (a + b));
+    printf("%lld * %lld = %lld\n", a, b, (a * b));
+    printf("%lld - %lld = %lld\n", a, b, (a - b));
+
+    return 0;
+}
+
+/*
+    PROBLEM LINK: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/C
+*/
