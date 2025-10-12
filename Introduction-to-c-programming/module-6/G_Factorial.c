4be1e572da09f2ed6df85b4631c7a32ca29ac3d3
ahadahamedakash <a.ahad2232@gmail.com>
2025-10-13 01:54:24 +0600

    Solved codeforces loop problem no: 9

---
+++ Introduction-to-c-programming/module-6/G_Factorial.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+
+    scanf("%d", &t);
+
+    for (int i = 1; i <= t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+
+        long long int fact = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            fact *= i;
+        }
+
+        printf("%lld\n", fact);
+    }
+
+    return 0;
+}
