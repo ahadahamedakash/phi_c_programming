dde6cfc909e0187f6465c6c39479cda30f333bf1
ahadahamedakash <a.ahad2232@gmail.com>
2025-10-18 23:35:37 +0600

    Remove a value from an arry by given index no

---
+++ Introduction-to-c-programming/module-9/2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int index;
+    scanf("%d", &index);
+
+    for (int i = index; i < n; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    // arr[n - 1] = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+/*
+    REMOVE A GIVEN INDEX VALUE FROM AN ARRAY
+*/
