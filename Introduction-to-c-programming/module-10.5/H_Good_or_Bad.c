16408d6d65b19e56d8f4158196b46dbbb9dbea31
ahadahamedakash <a.ahad2232@gmail.com>
2025-10-22 01:37:00 +0600

    Solved codeforces practice problem and refactored the good or bad printing problem

--- Introduction-to-c-programming/module-10.5/H_Good_or_Bad.c
+++ Introduction-to-c-programming/module-10.5/H_Good_or_Bad.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
@@ -9,40 +10,23 @@ int main()
     {
         char s[1000001];
 
-        scanf("%s", &s);
-        // printf("%s", s);
+        scanf("%s", s);
+
+        int isGood = false;
 
         for (int j = 0; j < strlen(s) - 2; j++)
         {
+            if ((s[j] == '1' && s[j + 1] == '0' && s[j + 2] == '1') || (s[j] == '0' && s[j + 1] == '1' && s[j + 2] == '0'))
+            {
+                printf("Good\n");
+                isGood = true;
+                break;
+            }
+        }
 
-            if (s[j] == '1')
-            {
-                if (s[j + 1] == '0')
-                {
-
-                    if (s[j + 2] == '1')
-                    {
-                        printf("Good\n");
-                        break;
-                    }
-                }
-            }
-            else if (s[j] == '0')
-            {
-                if (s[j + 1] == '1')
-                {
-                    if (s[j + 2] == '0')
-                    {
-                        printf("Good\n");
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                printf("Bad\n");
-                // break;
-            }
+        if (!isGood)
+        {
+            printf("Bad\n");
         }
     }
 
