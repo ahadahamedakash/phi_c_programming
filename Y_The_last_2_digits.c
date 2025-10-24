c53955553b761062cad97b72f6194dfe9cecf305
ahadahamedakash <a.ahad2232@gmail.com>
2025-10-24 21:54:33 +0600

    Solved codeforces practice problem

---
+++ Y_The_last_2_digits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b, c, d;
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+
+    long long int res = (a % 100) * (b % 100);
+    res = (res % 100);
+    res = (res * (c % 100)) % 100;
+    res = (res * (d % 100)) % 100;
+
+    printf("%lld", res);
+
+    return 0;
+}
