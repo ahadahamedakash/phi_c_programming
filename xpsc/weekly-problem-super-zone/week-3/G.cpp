4d27d11dc05a95418a6c485f63ec9cc623f025f3
ahadahamedakash <a.ahad2232@gmail.com>
2026-07-27 16:00:00 +0600

    Solved XPSC SW3 F G K & L

---
+++ xpsc/weekly-problem-super-zone/week-3/G.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define spc ' '
+#define imx INT_MAX
+#define imn INT_MIN
+#define llmx LLONG_MAX
+#define llmn LLONG_MIN
+#define ld long double
+#define ll long long int
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+#define sqr(x) ((x) * (x))
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main()
+{
+    fastIO();
+
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    vector<ll> v(m + 1);
+    for (ll &i : v)
+        cin >> i;
+
+    ll ans = 0;
+    for (int i = 0; i < m; ++i)
+    {
+        ll xxor = (v[i] ^ v[m]);
+
+        if (__popcount(xxor) <= k)
+            ans++;
+    }
+
+    cout << ans << nl;
+
+    return 0;
+}
---
+++ xpsc/weekly-problem-super-zone/week-3/L.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define spc ' '
+#define imx INT_MAX
+#define imn INT_MIN
+#define llmx LLONG_MAX
+#define llmn LLONG_MIN
+#define ld long double
+#define ll long long int
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+#define sqr(x) ((x) * (x))
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main()
+{
+    fastIO();
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        string ans = "";
+
+        int pairs, mustChange;
+        pairs = mustChange = 0;
+
+        int l = 0, r = n - 1;
+        while (l < r)
+        {
+            if (s[l] == s[r])
+                pairs += 2;
+            else
+                mustChange++;
+
+            l++, r--;
+        }
+
+        for (int i = 0; i <= n; ++i)
+        {
+            int total = i;
+            total -= mustChange;
+            if (total < 0)
+            {
+                ans.push_back('0');
+                continue;
+            }
+
+            total = max((total % 2), total - pairs);
+            total = max(0, total - (n % 2));
+
+            if (total == 0)
+                ans.push_back('1');
+            else
+                ans.push_back('0');
+        }
+
+        cout << ans << nl;
+    }
+
+    return 0;
+}
