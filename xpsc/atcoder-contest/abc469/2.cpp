4a590ea328324d7b55ae513981427b637f5e7e2d
ahadahamedakash <a.ahad2232@gmail.com>
2026-08-01 23:28:28 +0600

    Solved ABC469 A & B CF R1113 A

---
+++ xpsc/atcoder-contest/abc469/2.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define nl '\n'
+#define spc ' '
+#define imx INT_MAX
+#define imn INT_MIN
+#define llmx LLONG_MAX
+#define llmn LLONG_MIN
+#define ld long double
+#define ll long long int
+#define no cout << "NO\n"
+#define yes cout << "YES\n"
+#define sqr(x) ((x) * (x))
+#define sz(x) ((int)(x).size())
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fastIO() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+
+int main()
+{
+    fastIO();
+
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int ans = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (s[i] == 'o')
+            continue;
+
+        bool left = (i == 0 || s[i - 1] == 'x');
+        bool right = (i == n - 1 || s[i + 1] == 'x');
+
+        if (left && right)
+            ans++;
+    }
+
+    cout << ans << nl;
+
+    return 0;
+}
